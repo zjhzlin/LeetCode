cef6ea76292c75cee2dc62f8c2dd00022b12b7d6
zjhzlin <74384600+zjhzlin@users.noreply.github.com>
2021-06-21 23:06:20 +0100

    threesum updates

--- src/com/leetcode/ThreeSum.java
+++ src/com/leetcode/ThreeSum.java
@@ -9,43 +9,26 @@ public class ThreeSum {
 
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new LinkedList<>();
-
-        if(nums == null || nums.length < 3) {
-            return res;
-        }
-
         Arrays.sort(nums);
-
         for(int i = 0; i <= nums.length - 3; i++) {
-            if(i != 0 && nums[i] == nums[i-1]) ;
-            else {
+            if( i == 0 || (i > 0 && nums[i] != nums[i-1])) {
                 int left = i + 1;
                 int right = nums.length - 1;
-                while (left < right && right > i && left < nums.length) {
+                while (left < right) {
                     int sum = nums[i] + nums[left] + nums[right];
                     if (sum == 0) {
-                        List<Integer> ans = new ArrayList<>();
-                        ans.add(nums[i]);
-                        ans.add(nums[left]);
-                        ans.add(nums[right]);
-                        if(!res.contains(ans)) res.add(ans);
+                        res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left+1]) left++;
+                        while (left < right && nums[right] == nums[right-1]) right--;
                         left++;
                         right--;
                     }
-                    else if (sum > 0) {
-                        right--;
-                        if (right + 1 < nums.length && nums[right] == nums[right+1]) right--;
-                    }
+                    else if (sum > 0) right--;
+                    else left++;
 
-                    else {
-                        left++;
-                        if (left - 1 > i && nums[left] == nums[left-1]) left++;
-                    }
                 }
             }
-
         }
-
         return res;
     }
 
