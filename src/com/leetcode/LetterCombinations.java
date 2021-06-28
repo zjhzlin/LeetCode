c1ca16940a8a1de9a17b0308d380129805c6b5bd
zjhzlin <74384600+zjhzlin@users.noreply.github.com>
2021-06-28 07:29:38 +0100

    letter combination updates

--- src/com/leetcode/LetterCombinations.java
+++ src/com/leetcode/LetterCombinations.java
@@ -4,43 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LetterCombinations {
+    private static String[] dict = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
     public static List<String> letterCombinations(String digits) {
-        String[] phoneLetters = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        ArrayList<String> combinations = new ArrayList<>();
-        if(digits == null || digits.length() < 1) return combinations;
-        return dfs(digits, phoneLetters, combinations);
+        List<String> result = new ArrayList<>();
+        // corner case
+        if (digits.length() == 0) return result;
+        // depth first search
+        backtrack(result, digits.toCharArray(), "");
+        return result;
     }
 
-    private static List<String> dfs(String digits, String[] phoneLetters, List<String> combinations) {
+    public static void backtrack(List<String> combos, char[] digits, String s) {
         // base case
-        if (digits.length() == 1) {
-            int num = Integer.parseInt(String.valueOf(digits.charAt(0)));
-            String letters = phoneLetters[num];
-            combinations = combine(combinations, letters);
+        if (s.length() == digits.length) {
+            combos.add(s);
+            return;
         }
-
-        while (digits.length() > 1) {
-            int num = Integer.parseInt(String.valueOf(digits.charAt(0)));
-            String letters = phoneLetters[num];
-            List<String> comb = dfs(digits.substring(1), phoneLetters, combinations);
-            combinations = combine(comb, letters);
-            digits = digits.substring(1);
+        // depth first search
+        int i = s.length();
+        int digit = digits[i] - '0';
+        for (char letter : dict[digit].toCharArray()) {
+            backtrack(combos, digits, s + Character.toString(letter));
         }
-        return combinations;
-    }
-
-    private static List<String> combine(List<String> comb, String letters){
-        ArrayList<String> combinations = new ArrayList<>();
-        for (int i = 0; i < letters.length(); i++) {
-            if(comb.size() == 0) {
-                combinations.add(letters.substring(i,i+1));
-            }
-            for (String letter: comb) {
-                String newLetter = letters.charAt(i) + letter;
-                combinations.add(newLetter);
-            }
-        }
-        return combinations;
     }
 
     public static void main(String[] args) {
